36ab8ca3878748e44edf12cb48c86a6a1c98d29a
chaos44 <liuchao46642@yahoo.co.jp>
2022-04-08 22:10:49 +0900

    Add base admin interface

---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,41 @@
+import NextAuth from 'next-auth';
+// https://next-auth.js.org/providers/credentials#example
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from '../../../lib/prisma';
+import {verifyPassword} from "../../../lib/auth";
+
+export default NextAuth({
+    session: {
+        // jwt: true
+        strategy: "jwt",
+    },
+    providers: [
+        CredentialsProvider({
+            name: 'Calendso',
+            credentials: {
+                email: { label: "Email Address", type: "email", placeholder: "john.doe@example.com" },
+                password: { label: "Password", type: "password", placeholder: "Your super secure password" }
+            },
+            async authorize(credentials) {
+                const user = await prisma.user.findFirst({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                if (!user) {
+                    throw new Error('No user found');
+                }
+
+                const isValid = await verifyPassword(credentials.password, user.password);
+
+                if (!isValid) {
+                    throw new Error('Incorrect password');
+                }
+
+                return {id: user.id, username: user.username, email: user.email, name: user.name};
+            }
+        })
+    ],
+    secret: '123',
+});
