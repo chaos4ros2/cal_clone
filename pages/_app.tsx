bc4285f51d92566bc8b366b1317c6a903cce7e4a
chaos44 <liuchao46642@yahoo.co.jp>
2022-04-23 11:44:49 +0900

    Standardise semicolons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 // https://next-auth.js.org/getting-started/upgrade-v4#sessionprovider
 // import { Provider } from 'next-auth/client'
-import { SessionProvider  } from 'next-auth/react'
+import { SessionProvider  } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
   return (
       <SessionProvider  session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider >
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/api/integrations/googlecalendar/add.ts
+++ pages/api/integrations/googlecalendar/add.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 import prisma from '../../../../lib/prisma';
 const {google} = require('googleapis');
