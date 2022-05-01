38698d6dd4a205140c7033636cf1c2ce9fa977f9
chaos44 <liuchao46642@yahoo.co.jp>
2022-05-01 16:57:55 +0900

    update .env for production environment

---
+++ .env.ex
@@ -0,0 +1,11 @@
+# Environment variables declared in this file are automatically made available to Prisma.
+# See the documentation for more detail: https://pris.ly/d/prisma-schema#using-environment-variables
+
+# Prisma supports the native connection string format for PostgreSQL, MySQL and SQLite.
+# See the documentation for all the connection string options: https://pris.ly/d/connection-strings
+
+DATABASE_URL="postgres://giryoysskrvtvs:2963699685026db6e2d8aa34e461727452a3c56da321e0013911958a786ec981@ec2-54-147-203-50.compute-1.amazonaws.com:5432/d9lnpsm6lb5652"
+
+GOOGLE_API_CREDENTIALS={"web":{"client_id":"722451999687-05kkhsgo3ls1jdteearn1mqse1jl29bo.apps.googleusercontent.com","project_id":"ecstatic-motif-329705","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost:3000/api/integrations/googlecalendar/callback"],"javascript_origins":["http://localhost:3000"]}}
+
+NEXTAUTH_URL=http://localhost:3000
